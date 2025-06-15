ce87115df38adfc28df7e9d63f707d847d6f85fd
Rafael <rafael.quelho@gmail.com>
2025-06-15 08:23:12 -0300

    feat: boilerplate

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = 'Hello, webpack 2!';
+  return element;
+}
+
+document.getElementById('app').appendChild(component());
+
+if (module.hot) {
+  module.hot.accept('./index.js', function() {
+    console.log('The module was updated!');
+  });
+}
